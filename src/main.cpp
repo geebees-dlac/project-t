3f7f5b0d66661aa34b161dfdd02976a4501adb8a
Komi_ui <champlayersarte@gmail.com>
2025-05-09 16:34:34 +0800

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,46 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-
+ 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+ 
+    // Load a sprite to display
+    const sf::Texture texture("cute_image.jpg");
+    sf::Sprite sprite(texture);
+ 
+    // Create a graphical text to display
+    const sf::Font font("arial.ttf");
+    sf::Text text(font, "Hello SFML", 50);
+ 
+    // Load a music to play
+    sf::Music music("nice_music.ogg");
+ 
+    // Play the music
+    music.play();
+ 
+    // Start the game loop
     while (window.isOpen())
     {
+        // Process events
         while (const std::optional event = window.pollEvent())
         {
+            // Close window: exit
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
+ 
+        // Clear screen
         window.clear();
-        window.draw(shape);
+ 
+        // Draw the sprite
+        window.draw(sprite);
+ 
+        // Draw the string
+        window.draw(text);
+ 
+        // Update the window
         window.display();
     }
 }

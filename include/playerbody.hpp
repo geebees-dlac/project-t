39f7bc02a724df2e4ab40b1f07c526262bea5b7d
Komi_ui <champlayersarte@gmail.com>
2025-05-14 13:47:34 +0800

    updated player tiles trapbody and interpol

--- include/playerbody.hpp
+++ include/playerbody.hpp
@@ -1,1 +1,31 @@
 //dynamic body
+
+#ifndef DYNAMICBODY_HPP
+#define DYNAMICBODY_HPP
+
+////////////////////////////////////////////////////////////
+/// \brief This class was created to complement the code
+///////////////////////////////////////////////////////////
+
+#include <SFML/System/Vector2.hpp>
+
+namespace phys
+{
+	class dynamicBody
+	{
+	public:
+		dynamicBody();
+		~dynamicBody();
+
+	public:
+		int m_moveX;
+		int m_moveY;
+		sf::Vector2f m_lastPosition;
+		sf::Vector2f m_position;
+		sf::Vector2f m_velocity;
+		float m_width;
+		float m_height;
+	};
+}
+
+#endif //DYNAMICBODY_HPP
